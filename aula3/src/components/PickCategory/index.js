75331262a1603ab3397e04751bacae05cedb9f2d
Joao Pedro Bretanha <jpbretanha@gmail.com>
2019-06-29 16:44:45 -0300

    implement picker

--- aula3/src/components/PickCategory/index.js
+++ aula3/src/components/PickCategory/index.js
@@ -1,20 +1,55 @@
 import React from 'react'
-import { Picker } from "react-native"
+import { Picker, ActivityIndicator } from "react-native"
+import { getCategories } from '../../services/category'
 
 class PickCategory extends React.Component {
     state = {
-        language: ''
+        category: null,
+        isLoading: true,
+        hasError: false,
+        categories: []
     }
+
+    async componentDidMount () {
+        try {
+            const response = await getCategories()
+            this.setState({
+                categories: response.data
+            })
+        } catch (error) {
+            console.log(error)
+        } finally {
+            this.setState({
+                isLoading: false
+            })
+        }
+    }
+
     render () {
+        if (this.state.isLoading) {
+            return <ActivityIndicator color='#000' size='large' />
+        }
         return (
             <Picker
-                selectedValue={this.state.language}
+                selectedValue={this.state.category}
                 style={{height: 100}}
                 onValueChange={(itemValue, itemIndex) =>
-                    this.setState({language: itemValue})
+                    this.setState({category: itemValue})
                 }>
-                <Picker.Item label="Java" value="java" />
-                <Picker.Item label="JavaScript" value="js" />
+                    {
+                        this.state.categories.map(
+                            category => {
+                                return (
+                                    <Picker.Item
+                                        key={category.id}
+                                        label={category.name}
+                                        value={category.id}
+                                    />
+                                )
+                            }
+                        )
+                    }
+                
             </Picker>
         )
     }
