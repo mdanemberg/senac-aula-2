9c67e810f3fcbb697bde81fc471d2087d2e964f8
Joao Pedro Bretanha <jpbretanha@gmail.com>
2019-06-29 08:49:32 -0300

    create messages store and get

---
+++ aula4/database/index.js
@@ -0,0 +1,31 @@
+import { AsyncStorage } from 'react-native'
+
+const getPage = (messages = [], perPage = 10, page = 1) => {
+    const pages = messages.reduce((resultArray, item, index) => { 
+        const chunkIndex = Math.floor(index/perPage)
+
+        if(!resultArray[chunkIndex]) {
+            resultArray[chunkIndex] = []
+        }
+        resultArray[chunkIndex].push(item)
+
+        return resultArray
+    }, [])
+    return pages[(page - 1)]
+}
+
+export const store = async message => {
+    const messages = await AsyncStorage.getItem('messages')
+    await AsyncStorage.setItem('messages',
+        [message, ...(messages && messages)]
+    )
+}
+
+export const getMessages = async (page, perPage) => {
+    const messages = await AsyncStorage.getItem('messages')
+    return {
+        messages: messages ? getPage(messages, perPage, page) : [],
+        page,
+        perPage
+    }
+}
