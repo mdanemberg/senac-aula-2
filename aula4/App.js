e189834a728bf9733a908d086d1406945b83e0a1
Joao Pedro Bretanha <jpbretanha@gmail.com>
2019-06-29 08:07:23 -0300

    chat hooks

--- aula4/App.js
+++ aula4/App.js
@@ -1,19 +1,9 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer, createStackNavigator } from 'react-navigation'
+import Chat from './src/Chat'
+console.disableYellowBox = true
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+const RootStack = createStackNavigator({
+  Chat
+})
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default createAppContainer(RootStack)
---
+++ aula4/src/Chat.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react'
+import { GiftedChat } from 'react-native-gifted-chat'
+import Socket from 'socket.io-client'
+
+const ws = Socket('https://senac-shopping-list-api.herokuapp.com', {
+  transports: ['websocket'],
+  jsonp: false
+})
+
+// {
+//     _id: 1,
+//     text: 'My message',
+//     createdAt: new Date(),
+//     user: {
+//         _id: 2,
+//         name: 'React Native',
+//     }
+// }
+
+const Chat = () => {
+    const [messages, setMessages] = useState([])
+    const myId = 1
+
+    useEffect(() => {
+        ws.connect()
+        ws.on('message', message => {
+            if (myId !== message.user._id) {
+                setMessages(GiftedChat.append(messages, [message]))
+            }
+        })
+    }, [messages])
+
+    const sendMessage = newMessages => {
+        setMessages(GiftedChat.append(messages, newMessages))
+        ws.emit('message', newMessages[0])
+    }
+    console.log(messages)
+    return (
+        <GiftedChat
+            messages={messages}
+            onSend={sendMessage}
+            renderAvatar={null}
+            user={{
+                _id: myId
+            }}
+        />
+    )
+}
+
+export default Chat
