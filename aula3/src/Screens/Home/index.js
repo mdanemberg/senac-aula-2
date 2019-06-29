81e5ac8818fb66b3d3d56a4df6dfb9a3d6ccf9a4
Joao Pedro Bretanha <jpbretanha@gmail.com>
2019-06-29 16:56:00 -0300

    implement modal

--- aula3/src/Screens/Home/index.js
+++ aula3/src/Screens/Home/index.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Modal, Button, StatusBar, Platform } from 'react-native'
 import styled from 'styled-components'
 import { Picker } from '../../components'
 
 class Home extends Component {
+    state = {
+        isVisible: false
+    }
+
+    openModal = () => {
+        this.setState({
+            isVisible: !this.state.isVisible
+        })
+    }
+
     render () {
         return (
             <View>
-                <Picker />
+                <Button onPress={this.openModal} title='Abrir seletor' />
+                <Modal close={this.openModal} visible={this.state.isVisible}>
+                    <View
+                        style={{
+                            paddingTop:
+                            Platform.OS === 'android' 
+                                ? StatusBar.currentHeight : 20,
+                            flex: 1
+                        }}
+                    >
+                        <StatusBar />
+                        <Picker />
+                        <Button title='fechar' onPress={this.openModal} />
+                    </View>
+                </Modal>
             </View>
         )
     }
